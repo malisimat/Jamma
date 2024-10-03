4993ec634daa78ff99e96cc737a54c4a84a041aa
Matt Jones <matt.jones@perpetuum.com>
2024-10-03 01:04:34 +0100

    Add string replace utility

--- JammaLib/src/utils/StringUtils.h
+++ JammaLib/src/utils/StringUtils.h
@@ -10,6 +10,8 @@
 #include <string>
 #include <windows.h>
 #include <Rpc.h>
+#include <xlocbuf>
+#include <codecvt>
 
 #pragma comment(lib,"Rpcrt4.lib")
 
@@ -22,4 +24,7 @@ namespace utils
 	std::wstring DecodeUtf8(const std::string& str);
 
 	std::string GetGuid();
+
+	bool StringReplace(std::string& str, const std::string& from, const std::string& to);
+	bool StringReplace(std::wstring& str, const std::wstring& from, const std::wstring& to);
 }
