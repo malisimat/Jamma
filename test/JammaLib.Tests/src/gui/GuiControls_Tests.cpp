e013f2c2d87468138c6630976645e8615bd6a82e
malisimat <mattrumba@gmail.com>
2026-04-14 21:44:17 +0100

    Bolster the button tests and other controls

--- test/JammaLib.Tests/src/gui/GuiControls_Tests.cpp
+++ test/JammaLib.Tests/src/gui/GuiControls_Tests.cpp
@@ -2,6 +2,7 @@
 #include "gui/GuiButton.h"
 #include "gui/GuiToggle.h"
 #include "gui/GuiRadio.h"
+#include <stdexcept>
 
 using base::ActionReceiver;
 using gui::GuiButton;
@@ -39,6 +40,16 @@ namespace
 	private:
 		int _actionCount;
 		actions::GuiAction _lastAction;
+	};
+
+	class ThrowingGuiReceiver :
+		public ActionReceiver
+	{
+	public:
+		virtual actions::ActionResult OnAction(actions::GuiAction action) override
+		{
+			throw std::runtime_error("receiver failure");
+		}
 	};
 
 	static TouchAction MakeTouchAction(TouchAction::TouchState state, utils::Position2d position)
@@ -92,6 +103,44 @@ TEST(GuiButton, DisabledButtonIgnoresTouch) {
 	ASSERT_FALSE(res.IsEaten);
 }
 
+TEST(GuiButton, TouchOutsideBoundsIsIgnored) {
+	auto button = std::make_shared<GuiButton>(MakeButtonParams());
+
+	ASSERT_FALSE(button->OnAction(MakeTouchAction(TouchAction::TOUCH_DOWN, { -1, 10 })).IsEaten);
+	ASSERT_FALSE(button->OnAction(MakeTouchAction(TouchAction::TOUCH_DOWN, { 21, 10 })).IsEaten);
+	ASSERT_FALSE(button->OnAction(MakeTouchAction(TouchAction::TOUCH_DOWN, { 10, -1 })).IsEaten);
+	ASSERT_FALSE(button->OnAction(MakeTouchAction(TouchAction::TOUCH_DOWN, { 10, 21 })).IsEaten);
+}
+
+TEST(GuiButton, ExactBoundaryTouchesAreIgnored) {
+	auto button = std::make_shared<GuiButton>(MakeButtonParams());
+
+	ASSERT_FALSE(button->OnAction(MakeTouchAction(TouchAction::TOUCH_DOWN, { 0, 10 })).IsEaten);
+	ASSERT_FALSE(button->OnAction(MakeTouchAction(TouchAction::TOUCH_DOWN, { 10, 0 })).IsEaten);
+	ASSERT_FALSE(button->OnAction(MakeTouchAction(TouchAction::TOUCH_DOWN, { 20, 10 })).IsEaten);
+	ASSERT_FALSE(button->OnAction(MakeTouchAction(TouchAction::TOUCH_DOWN, { 10, 20 })).IsEaten);
+}
+
+TEST(GuiButton, NearBoundaryInteriorTouchesAreAccepted) {
+	auto button = std::make_shared<GuiButton>(MakeButtonParams());
+
+	ASSERT_TRUE(button->OnAction(MakeTouchAction(TouchAction::TOUCH_DOWN, { 1, 10 })).IsEaten);
+	ASSERT_TRUE(button->OnAction(MakeTouchAction(TouchAction::TOUCH_DOWN, { 10, 1 })).IsEaten);
+	ASSERT_TRUE(button->OnAction(MakeTouchAction(TouchAction::TOUCH_DOWN, { 19, 10 })).IsEaten);
+	ASSERT_TRUE(button->OnAction(MakeTouchAction(TouchAction::TOUCH_DOWN, { 10, 19 })).IsEaten);
+}
+
+TEST(GuiButton, ZeroSizedButtonNeverEatsTouch) {
+	auto params = MakeButtonParams();
+	params.Size = { 0, 0 };
+	params.MinSize = { 0, 0 };
+
+	auto button = std::make_shared<GuiButton>(params);
+
+	ASSERT_FALSE(button->OnAction(MakeTouchAction(TouchAction::TOUCH_DOWN, { 0, 0 })).IsEaten);
+	ASSERT_FALSE(button->OnAction(MakeTouchAction(TouchAction::TOUCH_DOWN, { 1, 1 })).IsEaten);
+}
+
 TEST(GuiToggle, TouchUpFlipsStateAndNotifiesReceiver) {
 	auto toggle = std::make_shared<GuiToggle>(MakeToggleParams(5, 7));
 	auto receiver = std::make_shared<MockedGuiReceiver>();
@@ -109,6 +158,77 @@ TEST(GuiToggle, TouchUpFlipsStateAndNotifiesReceiver) {
 	ASSERT_EQ(GuiAction::ACTIONELEMENT_TOGGLE, receiver->LastAction().ElementType);
 	ASSERT_EQ(7u, receiver->LastAction().Index);
 	ASSERT_EQ(GuiToggleParams::TOGGLE_ON, std::get<GuiAction::GuiInt>(receiver->LastAction().Data).Value);
+}
+
+TEST(GuiToggle, TouchUpOutsideAfterTouchDownDoesNotToggle) {
+	auto toggle = std::make_shared<GuiToggle>(MakeToggleParams(5, 7));
+	auto receiver = std::make_shared<MockedGuiReceiver>();
+	toggle->SetReceiver(receiver);
+
+	auto downRes = toggle->OnAction(MakeTouchAction(TouchAction::TOUCH_DOWN, { 10, 10 }));
+	auto upRes = toggle->OnAction(MakeTouchAction(TouchAction::TOUCH_UP, { 30, 30 }));
+
+	ASSERT_TRUE(downRes.IsEaten);
+	ASSERT_FALSE(upRes.IsEaten);
+	ASSERT_EQ(GuiToggleParams::TOGGLE_OFF, toggle->GetToggleState());
+	ASSERT_EQ(0, receiver->ActionCount());
+}
+
+TEST(GuiToggle, InterleavedTouchIndexesToggleDeterministically) {
+	auto toggle = std::make_shared<GuiToggle>(MakeToggleParams(5, 7));
+	auto receiver = std::make_shared<MockedGuiReceiver>();
+	toggle->SetReceiver(receiver);
+
+	TouchAction down0 = MakeTouchAction(TouchAction::TOUCH_DOWN, { 10, 10 });
+	down0.Index = 0;
+	TouchAction down1 = MakeTouchAction(TouchAction::TOUCH_DOWN, { 10, 10 });
+	down1.Index = 1;
+	TouchAction up0 = MakeTouchAction(TouchAction::TOUCH_UP, { 10, 10 });
+	up0.Index = 0;
+	TouchAction up1 = MakeTouchAction(TouchAction::TOUCH_UP, { 10, 10 });
+	up1.Index = 1;
+
+	ASSERT_TRUE(toggle->OnAction(down0).IsEaten);
+	ASSERT_TRUE(toggle->OnAction(down1).IsEaten);
+	ASSERT_TRUE(toggle->OnAction(up0).IsEaten);
+	ASSERT_TRUE(toggle->OnAction(up1).IsEaten);
+
+	ASSERT_EQ(GuiToggleParams::TOGGLE_OFF, toggle->GetToggleState());
+	ASSERT_EQ(2, receiver->ActionCount());
+}
+
+TEST(GuiToggle, RapidTapSequenceMaintainsConsistentParity) {
+	auto toggle = std::make_shared<GuiToggle>(MakeToggleParams(5, 7));
+	auto receiver = std::make_shared<MockedGuiReceiver>();
+	toggle->SetReceiver(receiver);
+
+	for (int i = 0; i < 9; ++i)
+	{
+		auto downRes = toggle->OnAction(MakeTouchAction(TouchAction::TOUCH_DOWN, { 10, 10 }));
+		auto upRes = toggle->OnAction(MakeTouchAction(TouchAction::TOUCH_UP, { 10, 10 }));
+		ASSERT_TRUE(downRes.IsEaten);
+		ASSERT_TRUE(upRes.IsEaten);
+	}
+
+	ASSERT_EQ(GuiToggleParams::TOGGLE_ON, toggle->GetToggleState());
+	ASSERT_EQ(9, receiver->ActionCount());
+}
+
+TEST(GuiToggle, ReceiverExceptionPropagatesAndControlRecovers) {
+	auto toggle = std::make_shared<GuiToggle>(MakeToggleParams(5, 7));
+	toggle->SetReceiver(std::make_shared<ThrowingGuiReceiver>());
+
+	ASSERT_TRUE(toggle->OnAction(MakeTouchAction(TouchAction::TOUCH_DOWN, { 10, 10 })).IsEaten);
+	EXPECT_THROW(toggle->OnAction(MakeTouchAction(TouchAction::TOUCH_UP, { 10, 10 })), std::runtime_error);
+	ASSERT_EQ(GuiToggleParams::TOGGLE_ON, toggle->GetToggleState());
+
+	auto recoveryReceiver = std::make_shared<MockedGuiReceiver>();
+	toggle->SetReceiver(recoveryReceiver);
+
+	ASSERT_TRUE(toggle->OnAction(MakeTouchAction(TouchAction::TOUCH_DOWN, { 10, 10 })).IsEaten);
+	ASSERT_TRUE(toggle->OnAction(MakeTouchAction(TouchAction::TOUCH_UP, { 10, 10 })).IsEaten);
+	ASSERT_EQ(GuiToggleParams::TOGGLE_OFF, toggle->GetToggleState());
+	ASSERT_EQ(1, recoveryReceiver->ActionCount());
 }
 
 TEST(GuiRadio, InitValueSetsSelectedChild) {
@@ -131,6 +251,46 @@ TEST(GuiRadio, InitValueSetsSelectedChild) {
 	ASSERT_EQ(GuiToggleParams::TOGGLE_OFF, first->GetToggleState());
 	ASSERT_EQ(GuiToggleParams::TOGGLE_ON, second->GetToggleState());
 	ASSERT_EQ(GuiToggleParams::TOGGLE_OFF, third->GetToggleState());
+}
+
+TEST(GuiRadio, OutOfRangeInitValueKeepsAllChildrenOff) {
+	GuiRadioParams params(MakeButtonParams(11));
+	params.InitValue = 99;
+	params.ToggleParams = {
+		MakeToggleParams(0, 0),
+		MakeToggleParams(1, 1),
+		MakeToggleParams(2, 2)
+	};
+
+	auto radio = std::make_shared<GuiRadio>(params);
+	radio->Init();
+
+	auto first = std::dynamic_pointer_cast<GuiToggle>(radio->TryGetChild(0));
+	auto second = std::dynamic_pointer_cast<GuiToggle>(radio->TryGetChild(1));
+	auto third = std::dynamic_pointer_cast<GuiToggle>(radio->TryGetChild(2));
+
+	ASSERT_EQ(99u, radio->CurrentValue());
+	ASSERT_EQ(GuiToggleParams::TOGGLE_OFF, first->GetToggleState());
+	ASSERT_EQ(GuiToggleParams::TOGGLE_OFF, second->GetToggleState());
+	ASSERT_EQ(GuiToggleParams::TOGGLE_OFF, third->GetToggleState());
+}
+
+TEST(GuiRadio, EmptyToggleParamsIgnoresChildGuiAction) {
+	GuiRadioParams params(MakeButtonParams(11));
+	params.InitValue = 7;
+
+	auto radio = std::make_shared<GuiRadio>(params);
+	radio->Init();
+
+	GuiAction action;
+	action.ElementType = GuiAction::ACTIONELEMENT_TOGGLE;
+	action.Index = 0;
+	action.Data = GuiAction::GuiInt(1);
+
+	auto res = radio->OnAction(action);
+
+	ASSERT_FALSE(res.IsEaten);
+	ASSERT_EQ(7u, radio->CurrentValue());
 }
 
 TEST(GuiRadio, ChildToggleUpdatesSelectionAndNotifiesReceiver) {
