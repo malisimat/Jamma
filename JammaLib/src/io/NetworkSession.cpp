f35dc981042f7920a4c7c02bdd9aae3ec131da96
malisimat <mattrumba@gmail.com>
2026-05-07 03:29:14 +0100

    Update session class to not require ninjam WDL

--- JammaLib/src/io/NetworkSession.cpp
+++ JammaLib/src/io/NetworkSession.cpp
@@ -1,18 +1,29 @@
 #include "NetworkSession.h"
 
-#include "WDL/jnetlib/util.h"
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#endif
 
 namespace io
 {
 	NetworkSession::NetworkSession() noexcept :
-		_initialised(JNL::open_socketlib() == 0)
+		_initialised(false)
 	{
+	#ifdef _WIN32
+		WSADATA wsaData{};
+		_initialised = (WSAStartup(MAKEWORD(2, 2), &wsaData) == 0);
+	#else
+		_initialised = true;
+	#endif
 	}
 
 	NetworkSession::~NetworkSession() noexcept
 	{
+	#ifdef _WIN32
 		if (_initialised)
-			JNL::close_socketlib();
+			WSACleanup();
+	#endif
 	}
 
 	bool NetworkSession::IsInitialised() const noexcept
