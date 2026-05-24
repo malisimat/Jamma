e582f01496bbbe88accb5f6495e1c2995c2e484e
malisimat <mattrumba@gmail.com>
2026-05-24 09:20:42 +0100

    Silence static analysis warnings due to Jammable constructor mismatch

--- JammaLib/src/base/Jammable.h
+++ JammaLib/src/base/Jammable.h
@@ -20,6 +20,8 @@ namespace base
 		public TweakableParams
 	{
 	public:
+		JammableParams() = default;
+		explicit JammableParams(GuiElementParams gui) : GuiElementParams(gui) {}
 		std::string Texture;
 	};
 
